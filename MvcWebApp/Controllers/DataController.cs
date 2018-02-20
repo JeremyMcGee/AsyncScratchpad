6936669533f84af2399b6cb0886dbb48eef859b7
Jeremy McGee <jeremy@familymcgee.com>
2018-02-20 15:40:58 +0000

    Add async Task<>-based call

--- MvcWebApp/Controllers/DataController.cs
+++ MvcWebApp/Controllers/DataController.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace MvcWebApp.Controllers
 {
     public class DataController : ApiController
     {
-        // GET: api/Data
+        [Route("api/Data")]
+        [HttpGet]
         public string Get(int delay)
         {
-            var slowService = new SlowService.SlowServiceClient();
-            return slowService.Delay(delay);
+            using (var slowService = new SlowService.SlowServiceClient())
+            {
+                return slowService.Delay(delay);
+            }
+        }
+
+        [Route("api/DataAsync")]
+        [HttpGet]
+        public async Task<string> GetWithTask(int delay)
+        {
+            using (var slowService = new SlowService.SlowServiceClient())
+            {
+                return await slowService.DelayAsync(delay);
+            }
         }
     }
 }
