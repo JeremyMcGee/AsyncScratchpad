e39df89c1c201f73cd5a24de5b5910d555646f49
Jeremy McGee <jeremy@familymcgee.com>
2018-02-20 15:15:39 +0000

    Add an MVC app

---
+++ MvcWebApp/Controllers/DataController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcWebApp.Controllers
+{
+    public class DataController : ApiController
+    {
+        // GET: api/Data
+        public string Get(int delay)
+        {
+            var slowService = new SlowService.SlowServiceClient();
+            return slowService.Delay(delay);
+        }
+    }
+}
